e5c0f44cbe8de02b984105f8f5d88bbd943150f6
kyeongseo <keongseo1234@gmail.com>
2023-11-12 23:50:08 +0900

    login

--- frontend/dream/lib/login.dart
+++ frontend/dream/lib/login.dart
@@ -14,8 +14,61 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Form(
-          child: ,
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              children: [
+                Container(
+                  height: 600,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.amber,
+                  child: const Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 30),
+                        Text('Login',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+                        )),
+                        SizedBox(height: 30),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text('Email',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                          )),
+                        ),
+                        TextField(
+                          style: TextStyle(fontSize: 30, color: Colors.red),
+                        ),
+                        SizedBox(height: 30),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text('password',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                          )),
+                        ),
+                        TextField(
+                          style: TextStyle(fontSize: 30, color: Colors.red),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Text('회원가입하기', style: TextStyle(fontSize: 15))
+                        ),
+                      ],
+                  
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+          ),
         ),
       ),
 
