5c6cc70b7765a91e0202b1823cdceddb626cdaed
kyeongseo <keongseo1234@gmail.com>
2023-11-23 17:24:42 +0900

    user login api

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,45 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
 
-// app.get('/', (req, res) => {
-//     res.send('Hello world!')
-// })
+import express from 'express';
+import mysql from 'mysql2';
+import dbconfig from './database.js';
+const connection = mysql.createConnection(dbconfig);
 
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
+const app = express();
 
-import { area, circumference } from './circle.js';
+app.use(express.json());
+app.use(express.urlencoded( {extended : false }));
 
-console.log(`지름이 4인 원의 면적 : ${area(4)}`);
-console.log(`지름이 4인 원의 면적 : ${circumference(4)}`);
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.send('root');
+})
+
+// app.get('/auth/login', (req, res) => {
+//     connection.query('select * from Users', (error, rows) => {
+//         if(error) throw error;
+//         console.log('User info is:', rows);
+//         res.send(rows);
+
+//     });
+// });
+app.post('/auth/login', (req, res) => {
+    var email = req.body.email;
+    var pw = req.body.password;
+    console.log(email);
+    console.log(pw);
+    connection.query('select * from Users where email = ? and password = ?', [email, pw], function(error, results, fields) {
+        if(error) throw error;
+        if(results.length > 0) {
+            console.log('success');
+            res.redirect('')
+        } else {
+            console.log('login failed');
+        }
+    })
+
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Express server listening on port ' + app.get('port'));
+});
