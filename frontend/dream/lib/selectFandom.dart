169ea7105bfc485259c4f27f24c13b3842ff9c66
kyeongseo <keongseo1234@gmail.com>
2023-12-01 15:33:17 +0900

    add circleavatar

--- frontend/dream/lib/selectFandom.dart
+++ frontend/dream/lib/selectFandom.dart
@@ -8,31 +8,41 @@ class SelectFandom extends StatefulWidget {
   State<SelectFandom> createState() => _SelectFandomState();
 }
 
-class _SelectFandomState extends State<SelectFandom> {
+class _SelectFandomState extends State<SelectFandom>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  late final Animation<AlignmentGeometry> _animation = Tween<AlignmentGeometry>(
+    begin: Alignment.bottomLeft,
+    end: Alignment.center,
+  ).animate(CurvedAnimation(parent: _controller, curve: Curves.decelerate));
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   // search field controller
   final fieldText = TextEditingController();
 
   void clearText() {
     fieldText.clear();
   }
- 
-  // Uri singerSearch_uri = Uri.http("http://localhost:3000/search/singer");
 
   Future singer(String singer) async {
+    final singerSearch_uri =
+        Uri.http('localhost:3000', '/search/artists', {'team': singer});
 
-    // 가수 이름 쿼리 파라미터
-    // final queryParams = {
-    //   'team' : singer
-    // };
-
-    final singerSearch_uri = Uri.http('localhost:3000', '/search/artists', {'team' : singer});
-    // http://localhost:3000/search/artists?team=${singer.text}
     try {
-      var res = await http.get(singerSearch_uri, 
-      headers: {'Content-Type' : 'application/json; charset=UTF-8'},
+      var res = await http.get(
+        singerSearch_uri,
+        headers: {'Content-Type': 'application/json; charset=UTF-8'},
       );
-
-    } catch(e) {
+    } catch (e) {
       print(e);
     }
   }
@@ -46,7 +56,7 @@ class _SelectFandomState extends State<SelectFandom> {
           child: Column(
             children: [
               Container(
-                height: 700,
+                height: 50,
                 width: MediaQuery.of(context).size.width,
                 child: TextFormField(
                   controller: fieldText,
@@ -66,6 +76,24 @@ class _SelectFandomState extends State<SelectFandom> {
                   textInputAction: TextInputAction.search,
                 ),
               ),
+              Container(
+                height: 650,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                    padding: EdgeInsets.all(150),
+                    child: AlignTransition(
+                        alignment: _animation,
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage:
+                                  AssetImage('assets/images/riize.jpeg'),
+                              radius: 40,
+                            ),
+                            Text('riize')
+                          ],
+                        ))),
+              ),
             ],
           ),
         ),
@@ -73,3 +101,16 @@ class _SelectFandomState extends State<SelectFandom> {
     );
   }
 }
+
+// class MyCustomPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     Paint paint = Paint();
+//   }
+
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+//     // TODO: implement shouldRepaint
+//     throw UnimplementedError();
+//   }
+// }
