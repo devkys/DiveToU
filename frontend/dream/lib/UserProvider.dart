6b8be2d08c3ebcff70127761bc636c94bb413614
kyeongseo <keongseo1234@gmail.com>
2024-01-01 20:04:23 +0900

    create userprovider

---
+++ frontend/dream/lib/UserProvider.dart
@@ -0,0 +1,23 @@
+import 'package:dream/User.dart';
+import 'package:flutter/material.dart';
+
+// 사용자 정보 관리하는 inheritedWidget 클래스 
+class UserProvider extends InheritedWidget {
+  final User user;
+
+  UserProvider({required this.user, required Widget child}) : super(child: child);
+
+  // 사용자 정보에 접근하는 정적 메서드
+  static User of(BuildContext context) {
+    final UserProvider? provider = context.dependOnInheritedWidgetOfExactType<UserProvider>();
+    if(provider == null) {
+      throw FlutterError('User Provider not found in context');
+    }
+    return provider.user;
+  }
+  
+  @override
+  bool updateShouldNotify(UserProvider oldWidget) {
+    return user != oldWidget.user;
+  }
+}
