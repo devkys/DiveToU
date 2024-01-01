1aa30403f919ead4d418444688d03bf10a0d939e
kyeongseo <keongseo1234@gmail.com>
2024-01-01 15:34:13 +0900

    add User class

---
+++ frontend/dream/lib/User.dart
@@ -0,0 +1,9 @@
+class User { 
+  final String email;
+  final String name;
+  final String img_url;
+  final bool secret;
+
+  User({required this.email, required this.name, required this.img_url, required this.secret});
+
+}
